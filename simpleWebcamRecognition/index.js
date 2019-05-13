f901ea7cffc4e766f1cab5bd005d1673bfedeed3
AndreasRef <andreasrefsgaard@hotmail.com>
2019-05-13 10:46:05 +0200

    Attempting to make a p5js version

---
+++ simpleWebcamRecognition/index.js
@@ -0,0 +1,87 @@
+//See https://github.com/justadudewhohacks/face-api.js/issues/130
+
+let labeledFaceDescriptors;
+
+async function run1() {
+    const MODELS = "/models"; // Contains all the weights.
+
+    //await faceapi.loadSsdMobilenetv1Model(MODELS)
+    await faceapi.loadTinyFaceDetectorModel(MODELS)
+    await faceapi.loadFaceLandmarkModel(MODELS)
+    await faceapi.loadFaceRecognitionModel(MODELS)
+
+    console.log("models loaded, calculating labeledFaceDescriptors")
+
+    const labels = ['sheldon','howard', 'andreas', 'guy_with_hat']
+    const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 160 })
+
+
+    labeledFaceDescriptors = await Promise.all(
+        labels.map(async label => {
+            //console.log("starting labeledFaceDescriptors function")
+
+            // fetch image data from urls and convert blob to HTMLImage element
+            const imgUrl = `${label}.jpg`
+            const img = await faceapi.fetchImage(imgUrl)
+
+            // detect the face with the highest score in the image and compute it's landmarks and face descriptor
+            const fullFaceDescription = await faceapi.detectSingleFace(img, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptor()
+
+            if (!fullFaceDescription) {
+                throw new Error(`no faces detected for ${label}`)
+            }
+
+            const faceDescriptors = [fullFaceDescription.descriptor]
+            
+            return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
+        })     
+    )
+    console.log("Calculated all labeledFaceDescriptors ")
+
+    // try to access users webcam and stream the images
+    // to the video element
+    const videoEl = document.getElementById('inputVideo')
+    navigator.getUserMedia(
+        { video: {} },
+        stream => videoEl.srcObject = stream,
+        err => console.error(err)
+    )
+    console.log("Starting video")
+}
+
+async function recognise() {
+
+        const input = document.getElementById('inputVideo')
+        const fullFaceDescriptions = await faceapi.detectAllFaces(input, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors()
+
+        // 0.6 is a good distance threshold value to judge
+        // whether the descriptors match or not
+        const maxDescriptorDistance = 0.6
+        const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
+        //console.log("face matcher"+faceMatcher)
+        const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
+
+
+        const boxesWithText = results.map((bestMatch, i) => {
+            const box = fullFaceDescriptions[i].detection.box
+            const text = bestMatch.toString()
+            const boxWithText = new faceapi.BoxWithText(box, text)
+            //console.log(box._x)
+            return boxWithText
+        })
+
+        //Clear canvas
+        let myCanvas = document.getElementById('overlay');
+        const context = myCanvas.getContext('2d');
+        context.clearRect(0, 0, myCanvas.width, myCanvas.height);
+
+        faceapi.drawDetection(overlay, boxesWithText)
+}
+
+async function onPlay(videoEl) {
+    await recognise()
+    setTimeout(() => onPlay(videoEl))
+} 
+
+
+
